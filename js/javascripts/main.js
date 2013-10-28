fe35eb98e83fd704bb2a9dd8d89e3995a38079b5
naush <li-hsuan@namely.com>
2013-10-28 01:53:11 -0400

    Customization via url parameters

--- js/javascripts/main.js
+++ js/javascripts/main.js
@@ -1,6 +1,25 @@
 window.onload = function() {
-  var width = 30;
-  var height = 45;
+  var width = 10;
+  var height = 15;
+  var interval = 200;
+  var period = 100;
+
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var index = 0; index < vars.length; index = index + 1) {
+    var pair = vars[index].split('=');
+    var param = decodeURIComponent(pair[0]);
+    if (param == 'width') {
+      width = decodeURIComponent(pair[1]);
+    } else if (param == 'height') {
+      height = decodeURIComponent(pair[1]);
+    } else if (param == 'interval') {
+      interval = decodeURIComponent(pair[1]);
+    } else if (param == 'period') {
+      period = decodeURIComponent(pair[1]);
+    }
+  }
+
   var world = World.build(width, height);
   world = World.randomize(world, width, height);
   var tick = function(world, width, height, iteration) {
@@ -11,7 +30,7 @@ window.onload = function() {
       if (iteration > 0) {
         tick(world, width, height, iteration);
       }
-    }, 100);
+    }, interval);
   };
-  tick(world, width, height, 1000);
+  tick(world, width, height, period);
 };
